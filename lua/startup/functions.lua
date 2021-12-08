0567e98b3249d916679a1df1e984026145f186d5
max397574 <max.heinzer397@gmx.ch>
2021-12-08 14:25:39 +0100

    feat(functions): added time/date and annotations

--- lua/startup/functions.lua
+++ lua/startup/functions.lua
@@ -1,13 +1,21 @@
+---@brief [[
+---@tag startup.nvim_functions
+--- This are functions which you can use to build your own startup.nvim theme.
+---@brief ]]
 local functions = {}
 
 local quotes = require("startup.quotes")
 
+--- Returns a random programming quote
+---@return table quote Lines of text for the quote
 function functions.quote()
   math.randomseed(os.clock())
   local index = math.random() * #quotes
   return quotes[math.floor(index) + 1]
 end
 
+--- Returns a string with the number of loaded Packer Plugins
+---@return string PluginInfo String with info about the number of loaded plugins
 function functions.packer_plugins()
   return {
     string.format(
@@ -15,6 +23,14 @@ function functions.packer_plugins()
       vim.tbl_count(packer_plugins)
     ),
   }
+end
+
+--- Returns the current date and time
+---@return table date_time Table with a string for the date and one for the time
+function functions.date_time()
+  local clock = " " .. os.date("%H:%M")
+  local date = " " .. os.date("%d-%m-%y")
+  return { clock, date }
 end
 
 return functions
