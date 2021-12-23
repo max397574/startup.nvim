b24f337fb9c3257b5bbdf1e7679447ab9cdf034b
max397574 <max.heinzer397@gmx.ch>
2021-12-23 15:33:30 +0100

    feat(themes): increased paddings in evil

--- lua/startup/themes/evil.lua
+++ lua/startup/themes/evil.lua
@@ -105,7 +105,7 @@ local settings = {
     cursor_column = 0.5,
     empty_lines_between_mappings = true,
     disable_statuslines = true,
-    paddings = { 2, 1, 1, 1, 1, 1, 1 },
+    paddings = { 2, 2, 2, 2, 2, 2, 2 },
   },
   colors = {
     background = "#1f2227",
